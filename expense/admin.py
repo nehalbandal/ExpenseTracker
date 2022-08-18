7f60137d016b6adc7bd4a00907daab92e4fdf98b
Nehal Bandal <nihalbandal9@gmail.com>
2022-08-18 19:28:47 +0530

    updated

--- expense/admin.py
+++ expense/admin.py
@@ -1,4 +1,9 @@
+import csv
+import time
+
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import *
 from django.contrib.admin.filters import AllValuesFieldListFilter
 from .forms import CollectionForm
@@ -18,6 +23,25 @@ class MoneyAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     # list_editable = ['amount']
     # form = CollectionForm
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        field_names = ['building', 'flat_no', 'name', 'amount', 'status', 'note', 'attachment', 'added_by',
+                       'modified_by', 'creation_date', 'modification_date', ]
+
+        response = HttpResponse(content_type='text/csv')
+        datetime = time.strftime("%Y%m%d_%H%M%S")
+        filename =  "money_collection_" + datetime
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(filename)
+        writer = csv.writer(response)
+        print(field_names)
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
     def get_flat_no(self, obj):
         return "{}-{}".format(obj.building, obj.flat_no)
@@ -51,6 +75,26 @@ class ExpenseAdmin(admin.ModelAdmin):
         return int(round(obj.amount))
 
     get_amount.short_description = 'Amount(₹)'
+
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        field_names = ['expense_name', 'expense_desc', 'amount', 'expense_owner', 'note', 'attachment', 'added_by',
+                       'modified_by', 'creation_date', 'modification_date', ]
+
+        response = HttpResponse(content_type='text/csv')
+        datetime = time.strftime("%Y%m%d_%H%M%S")
+        filename = "expenses_" + datetime
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(filename)
+        writer = csv.writer(response)
+        print(field_names)
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
     def save_model(self, request, obj, form, change):
         instance = form.save(commit=False)
