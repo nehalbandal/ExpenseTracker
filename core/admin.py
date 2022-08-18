dd8fd4acba654cb2837d343b3439ad5e9944735c
Nehal Bandal <nihalbandal9@gmail.com>
2022-08-18 17:58:17 +0530

    updated

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username',)
+    search_fields = ()
+    list_filter = []
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(LogEntry)
 admin.site.site_header = 'Kamal Residency: Ganpati 2022'
 admin.site.index_title = 'Money Management'
