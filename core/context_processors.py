4619b643580c25d8c5fe832cea4ff03591f28a18
Nehal Bandal <nihalbandal9@gmail.com>
2022-08-23 18:58:37 +0530

    updated

--- core/context_processors.py
+++ core/context_processors.py
@@ -7,8 +7,15 @@ from expense.models import MoneyCollection, Expense
 
 def settings_media(request):
     total_expense = Expense.objects.all().aggregate(Sum('amount'))
+    total_expense = total_expense.get('amount__sum', 0) if total_expense.get('amount__sum', 0) else 0
+
     total_collection = MoneyCollection.objects.filter(status='PAID').aggregate(Sum('amount'))
     total_collection = total_collection.get('amount__sum', 0) if total_collection.get('amount__sum', 0) else 0
-    total_expense = total_expense.get('amount__sum', 0) if total_expense.get('amount__sum', 0) else 0
-    return {'setting': settings, 'total_collection': total_collection, 'total_expense': total_expense, }
+
+    total_vargani_collection = MoneyCollection.objects.filter(type='VARGANI', status='PAID').aggregate(Sum('amount'))
+    total_vargani_collection = total_vargani_collection.get('amount__sum', 0) if total_vargani_collection.get('amount__sum', 0) else 0
+    total_vargani_count = MoneyCollection.objects.filter(type='VARGANI', status='PAID').count()
+
+    return {'setting': settings, 'total_collection': total_collection, 'total_expense': total_expense,
+            'total_vargani_collection':total_vargani_collection, 'total_vargani_count': total_vargani_count}
 
